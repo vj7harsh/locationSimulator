f3b396a6c87fc0c04b84b374e9610f8260e779a3
vj7harsh <vhvijayharsha@gmail.com>
2021-06-26 02:30:02 +0530

    init

---
+++ src/test/java/com/locus/locationsimulator/LocationSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locus.locationsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
